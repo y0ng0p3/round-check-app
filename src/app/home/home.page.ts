2caf1daec40282f52681102f114cc84217921cd8
vyongope <vaillantyomen@gmail.com>
2022-11-08 05:31:22 +0100

    add geolocation plugin. create io service

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { BarcodeScanner, BarcodeScannerOptions } from '@awesome-cordova-plugins/barcode-scanner/ngx';
+import { DataService } from '../services/data/data.service';
+import { IoService } from '../services/io/io.service';
 
 
 @Component({
@@ -12,7 +14,7 @@ export class HomePage {
   scannedQRCode: any;
   barcodeScannerOptions: BarcodeScannerOptions;
 
-  constructor(private scanner: BarcodeScanner) {
+  constructor(private scanner: BarcodeScanner, private ds: DataService, private io: IoService) {
     this.encodedData = 'y0ng0p3';
 
     this.barcodeScannerOptions = {
@@ -22,8 +24,12 @@ export class HomePage {
   }
 
   scanQRcode() {
-    this.scanner.scan().then(res => {
+    this.scanner.scan().then(async res => {
       this.scannedQRCode = res;
+      // await this.ds.getQRcodes();
+      // const found = this.ds.qrcodeList.value.find((_) => _.thumb === res);
+      await this.ds.addCheck(1, `latitude:${this.io.latitude}, longitude:${this.io.latitude}`, false);
+      // alert({checks: this.ds.checkList});
     }).catch(err => {
       alert(err);
     });
@@ -32,7 +38,9 @@ export class HomePage {
   generateQRCode() {
     this.scanner.encode(this.scanner.Encode.TEXT_TYPE, this.encodedData).then(
         res => {
-          alert(res);
+          // alert(res);
+          console.log({res});
+        this.ds.addQRCode(this.encodedData, `latitude:${this.io.latitude}, longitude:${this.io.latitude}`, res);
           this.encodedData = res;
         }, error => {
           alert(error);
